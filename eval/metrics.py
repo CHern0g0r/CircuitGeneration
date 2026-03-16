a5453f0dfb3cd4c2c3ba1a4f7b4377e764bf27ca
Fedor Chernogorskii <fyodorchernogor@gmail.com>
2026-03-16 11:55:00 +0400

    conditional metrics

--- eval/metrics.py
+++ eval/metrics.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 from Levenshtein import ratio as levenshtein_ratio
+from tqdm import tqdm
+
+from datasets.src.utils import nx2cirbo
 
 
 def sort_tts(tts):
@@ -9,12 +12,42 @@ def sort_tts(tts):
     )
 
 
-def validity(is_valid):
-    return sum(is_valid) / len(is_valid)
+def nx_valid(nx_digraph):
+    pass
+
+
+def validity(graph_list):
+    circuit_list = []
+    is_valid = []
+    tts = []
+    for _, g in enumerate(tqdm(graph_list)):
+        try:
+            circuit = nx2cirbo(g)
+            circuit_list.append(circuit)
+
+            tt = circuit.get_truth_table()
+            tt_str = ''.join(
+                ''.join(str(int(bit)) for bit in inp)
+                for inp in tt
+            )
+            if not tt_str:
+                raise ValueError('Empty truth table')
+            is_valid.append(1)
+            tts.append(tt_str)
+        except Exception as e:
+            print(f"graph {_}: {type(e).__name__} {e}")
+            circuit_list.append('')
+            is_valid.append(0)
+            tts.append('')
+    validity_score = sum(is_valid) / len(is_valid)
+    return circuit_list, is_valid, tts, validity_score
 
 
 def uniqueness(tts):
+    tts = [tt for tt in tts if tt]
     unq = np.unique(tts)
+    if not tts:
+        return 0
     return len(unq) / len(tts)
 
 
@@ -22,6 +55,8 @@ def novelty(tts, train_tts):
     unq = set(sort_tts(tts))
     train_tts = set(sort_tts(train_tts))
     novel = [tt for tt in unq if tt not in train_tts]
+    if not unq:
+        return 0
     return len(novel) / len(unq)
 
 
@@ -33,11 +68,21 @@ def cond_validity(tts, cond_tts):
 def tt_distance(tts, cond_tts):
     res = []
     for tt, cond_tt in zip(tts, cond_tts):
-        n_inp = len(tt.split('_'))
-        cond_n_inp = len(cond_tt.split('_'))
+        if not tt:
+            continue
+        # print(f'{tt} vs {cond_tt}', len(tt), len(cond_tt))
+        n_inp = len(tt)
+        cond_n_inp = len(cond_tt)
 
         if n_inp != cond_n_inp:
             res.append(0)
         else:
             res.append(levenshtein_ratio(tt, cond_tt))
+    if not res:
+        return 0
     return sum(res) / len(res)
+
+
+def tt_em(tts, cond_tts):
+    correct = [1 if tt == cond_tt else 0 for tt, cond_tt in zip(tts, cond_tts)]
+    return sum(correct) / len(correct)
--- eval/run.py
+++ eval/run.py
@@ -1,47 +1,54 @@
 import pickle
 import pandas as pd
+import torch
 
-from datasets.src.utils import nx2cirbo
 from eval.metrics import (
     validity,
     uniqueness,
-    novelty
+    novelty,
+    tt_distance,
+    tt_em
 )
+from tqdm import tqdm
 
 
-def main(graphs, df):
+def main(graphs, df, labels=None):
     cond_tts, graph_list = zip(*graphs)
-    tts = df['circuit_name'].tolist()
 
-    circuit_list = []
-    is_valid = []
-    tts = []
-    for g in graph_list:
-        try:
-            circuit = nx2cirbo(g)
-            circuit_list.append(circuit)
-            is_valid.append(1)
+    circuit_list, is_valid, tts, validity_score = validity(graph_list)
+    print(len(tts))
 
-            tt = circuit.get_truth_table()
-            tt_str = '_'.join(
-                ''.join(str(int(bit)) for bit in inp)
-                for inp in tt
-            )
-            tts.append(tt_str)
-        except Exception:
-            circuit_list.append('')
-            is_valid.append(0)
-            tts.append('')
-
-    print(f'Validity: {validity(is_valid):.4f}')
+    print('Total', len(graph_list), 'valid', sum(is_valid))
+    print(f'Validity: {validity_score:.4f}')
     print(f'Uniqueness: {uniqueness(tts):.4f}')
     print(f'Novelty: {novelty(tts, df["circuit_name"].tolist()):.4f}')
+    # print(f'TT average distance: {tt_distance(tts, labels):.4f}')
+    # print(f'TTEM: {tt_em(tts, labels):.4f}')
+
+
+def torch_tt_tostr(tensor):
+    tensor = (tensor[tensor.nonzero()].squeeze() + 1) / 2
+    tensor = ''.join(str(int(bit)) for bit in tensor.tolist())
+    return tensor
 
 
 if __name__ == "__main__":
-    graphs_pth = 'circ/data/nx_generated.pkl'
-    df_path = 'data/circ/pickle/cirbo_small/cirbo_small_df.csv'
+
+    graphs_pth = ''
+    labels_pth = ''
+
+    df_pth = ''
 
     graphs = pickle.load(open(graphs_pth, 'rb'))
-    df = pd.read_csv(df_path)
-    main(graphs, df)
+    if not isinstance(graphs[0], tuple):
+        graphs = [(None, g) for g in graphs]
+    df = pd.read_csv(df_pth)
+
+    if labels_pth is not None:
+        labels = pickle.load(open(labels_pth, 'rb'))
+        if isinstance(labels[0], torch.Tensor):
+            labels = list(map(torch_tt_tostr, labels))
+        # for lab in labels:
+        #     print(lab)
+
+    main(graphs, df, labels)
